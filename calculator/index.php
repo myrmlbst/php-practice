c9208464406adf46f7164077c61a8e586d5da6d2
myriam <myriamalbustany@gmail.com>
2024-06-29 00:56:35 +0300

    initial commit

---
+++ calculator/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="">
+    <title>PHP Calculator</title>
+</head>
+
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+        <input type="number" name="num01" placeholder="First number">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num02" placeholder="Second number">
+
+        <button>
+            Calculate
+        </button>
+    </form>
+</body>
+
+</html>
